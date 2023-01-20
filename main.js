c3703a92cedbb3907d34de6b97099c9ba19b708b
islam mo'men <islam-hussein@outlook.com>
2023-01-20 20:27:36 +0300

    submit

---
+++ main.js
@@ -0,0 +1,51 @@
+// toggle menu
+let menuBtn = document.querySelector(".menu-btn");
+let links = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", btn => {
+    menuBtn.classList.toggle("menu-active");
+    links.classList.toggle("open-nav")
+});
+// filter
+let companies = document.querySelectorAll(".company-list li");
+let products = document.querySelectorAll(".card");
+
+companies.forEach(function(company){
+    let brand = company.dataset.filter;
+
+    company.addEventListener("click", function(e){
+        products.forEach(function(product){
+            let productBrand = product.dataset.company;
+
+            if (brand == "all") {
+                product.style.display = "block";
+            } else {
+                if(productBrand == brand){
+                    product.style.display = "block"
+                } else {
+                    product.style.display = "none";
+                }
+            }          
+        })
+    })
+})
+
+// search function
+let searchScreen = document.querySelector(".search input");
+let productNames = document.querySelectorAll(".product-name");
+
+searchScreen.addEventListener("keyup", function(){
+    productNames.forEach(function(item){
+
+        let searchValue = searchScreen.value.trim();
+        let lenght = searchValue.length;
+        let match = item.innerHTML.slice(0, lenght);
+
+        if (searchValue == match) {
+            item.parentNode.parentElement.style.display = "block";
+        } else{
+            item.parentNode.parentElement.style.display = "none"
+        }
+        
+    })
+})
